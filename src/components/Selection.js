5a5a8afd1fd7560938b23b2027c345652ef015fc
nirbhay kaushik <40763483+Nirbhay91@users.noreply.github.com>
2022-06-27 07:44:41 +0530

    update file

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,18 @@
+import React, { useState } from "react";
 
+import "../styles/Child.css";
+
+export default function Selection({ applyColor }) {
+  const [color, setColor] = useState({ background: "" });
+
+  function handleClick() {
+    applyColor(setColor);
+    console.log("done");
+  }
+
+  return (
+    <div className="flex-box" style={color} onClick={handleClick}>
+      <h2 className="subheding"> Selection </h2>
+    </div>
+  );
+}
